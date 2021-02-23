beaa375986fc32fca04cc9c12877f82c217aae43
M.Bezpalko <not_chudik16@yahoo.com>
2021-02-23 11:13:15 +0200

    refresh 23.02.2021

--- src/task3and4/Words.java
+++ src/task3and4/Words.java
@@ -1,45 +1,52 @@
 package task3and4;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Words {
     public static void main(String[] args) {
+        int wordsCount = 0;
+        int maxRepeats = 0;
+        int wordIndex = 0;
 
         System.out.println("Введіть речення щоб підрахувати кількість слів у ньому.");
         Scanner sc = new Scanner(System.in);
-        String test = sc.nextLine().toLowerCase();
-        String[] splitedTest = test.split(" ");
+        String test = sc.nextLine();
 
-        int wordsCount = 0;
-        for (int i = 0; i < splitedTest.length; i++) {
-            if (splitedTest[i].length() > 1)
-                wordsCount++;
+        ArrayList<String> splitter =
+                new ArrayList<>(Arrays.asList(test.split(" ")));
+
+        for (int i = 0; i < splitter.size(); i++) {
+            if (splitter.get(i).length() < 2) splitter.remove(i);
         }
-        System.out.println("Всього введено слів: " + wordsCount);
 
-        int[] reapeatedWords = new int[splitedTest.length];
-
-        for(int i=0; i<splitedTest.length; i++){
-            for(int j = i+1; j<splitedTest.length; j++){
-                if(splitedTest[i].equals(splitedTest[j])) {
-                     reapeatedWords[i]++;
-                }
+        for (int i = 0; i < splitter.size(); i++) {
+            if (splitter.get(i).length() > 1) {
+                wordsCount++;
             }
         }
-        int maxRepeats = 0;
-        int wordIndex = 0;
-        for(int i = 0; i<reapeatedWords.length; i++){
-            if(reapeatedWords[i]> maxRepeats) {
+
+        System.out.println("Всього введено слів: " + wordsCount);
+
+        int[] reapeatedWords = new int[splitter.size()];
+
+        for (int i = 0; i < splitter.size(); i++) {
+            for (int j = i + 1; j < splitter.size(); j++) {
+                if (splitter.get(j).equals(splitter.get(i))) reapeatedWords[i]++;
+            }
+        }
+        for (int i = 0; i < reapeatedWords.length; i++) {
+            if (reapeatedWords[i] > maxRepeats) {
                 maxRepeats = reapeatedWords[i];
                 wordIndex = i;
             }
         }
-
         System.out.println("Найчастіше було введено слово: "
-                         + splitedTest[wordIndex].toUpperCase() + " - "
-                         + (maxRepeats+1));
-
+                + splitter.get(wordIndex) + " - "
+                + (maxRepeats + 1));
         sc.close();
     }
 }
